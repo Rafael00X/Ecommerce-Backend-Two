df655fa66c0be227ed7b4ba07ce4cb1071ad334c
unknown <amsubhadeep@gmail.com>
2023-01-28 14:33:25 +0530

    Added custom exceptions

--- src/main/java/com/website/backendtwo/service/UserService.java
+++ src/main/java/com/website/backendtwo/service/UserService.java
@@ -1,6 +1,9 @@
 package com.website.backendtwo.service;
 
 import com.website.backendtwo.entity.User;
+import com.website.backendtwo.exception.EmailAlreadyRegisteredException;
+import com.website.backendtwo.exception.EmailNotRegisteredException;
+import com.website.backendtwo.exception.InvalidCredentialException;
 import com.website.backendtwo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +15,17 @@ public class UserService {
 
     public User registerUser(User user) {
         User existingUser = repository.findByEmail(user.getEmail()).orElse(null);
-        if (existingUser != null) return null;
+        if (existingUser != null) throw new EmailAlreadyRegisteredException();
         return repository.save(user);
     }
 
     public User loginUser(User user) {
         User existingUser = repository.findByEmail(user.getEmail()).orElse(null);
-        if (existingUser == null) return null;
-        if (existingUser.getPassword().equals(user.getPassword()))
-            return existingUser;
-        return null;
+        if (existingUser == null)
+            throw new EmailNotRegisteredException();
+        if (!existingUser.getPassword().equals(user.getPassword()))
+            throw new InvalidCredentialException();
+        return existingUser;
     }
 
 }
