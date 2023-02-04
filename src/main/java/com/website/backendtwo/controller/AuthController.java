38866acb2600d0bf1f68991afe5122f101d3a875
unknown <amsubhadeep@gmail.com>
2023-02-04 19:18:06 +0530

    Bug fixes

--- src/main/java/com/website/backendtwo/controller/AuthController.java
+++ src/main/java/com/website/backendtwo/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.website.backendtwo.controller;
 
 import com.website.backendtwo.entity.User;
+import com.website.backendtwo.exception.FailedRequestException;
 import com.website.backendtwo.exception.NotAuthorizedException;
 import com.website.backendtwo.service.CartService;
 import com.website.backendtwo.service.UserService;
@@ -23,17 +24,16 @@ public class AuthController {
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         User newUser = userService.registerUser(user);
-        if (newUser != null) {
-            newUser.setToken("user-token");
-            cartService.addCartOfUser(newUser);
-        }
+        if (newUser == null) throw new FailedRequestException("Failed to register user");
+        cartService.addCartOfUser(newUser);
+        newUser.setToken(JwtHelper.encode(newUser));
         return newUser;
     }
 
     @PostMapping("/login")
     public User loginUser(@RequestBody User user) {
         User existingUser = userService.loginUser(user);
-        if (existingUser != null) existingUser.setToken("user-token");
+        if (existingUser != null) existingUser.setToken(JwtHelper.encode(existingUser));
         return existingUser;
     }
 
--- src/main/java/com/website/backendtwo/utility/JwtHelper.java
+++ src/main/java/com/website/backendtwo/utility/JwtHelper.java
@@ -4,33 +4,40 @@ import com.website.backendtwo.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
+import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Objects;
 
 public class JwtHelper {
-    private static final String SECRET_KEY = "secret-key";
+    private static final String SECRET_KEY = "4428472B4B6250655368566D5971337436763979244226452948404D63516654";
+
+    private static Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public static String encode(User user) {
         return Jwts.builder()
-                .claim("email", user.getEmail())
-                .claim("password", user.getPassword())
-                .claim("userId", user.getUserId())
+                .setSubject(user.getUserId().toString())
+                .signWith(getSignInKey())
                 .setIssuedAt(Date.from(Instant.now()))
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
                 .compact();
     }
 
     public static Jws<Claims> decode(String jwt) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
                 .parseClaimsJws(jwt);
     }
 
     public static Boolean validate(String jwt, Integer userId) {
         Jws<Claims> result = decode(jwt);
-        return Objects.equals(userId, result.getBody().get("userId", Integer.class));
+        Integer tokenUserId = Integer.parseInt(result.getBody().getSubject());
+        return Objects.equals(userId, tokenUserId);
     }
 }
