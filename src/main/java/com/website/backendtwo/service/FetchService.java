363c8728e6afedfadc6ec19568d19f0b79a461c0
unknown <amsubhadeep@gmail.com>
2023-02-06 21:24:36 +0530

    Moved fetch url to application.properties

--- src/main/java/com/website/backendtwo/service/FetchService.java
+++ src/main/java/com/website/backendtwo/service/FetchService.java
@@ -2,6 +2,7 @@ package com.website.backendtwo.service;
 
 import com.website.backendtwo.entity.embeddable.Product;
 import com.website.backendtwo.exception.FailedRequestException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,15 @@ import java.util.List;
 
 @Service
 public class FetchService {
-
-    private static final String URL = "http://localhost:8081";
-    private RestTemplate restTemplate = new RestTemplate();
+    @Value("${backend-one.api.url}")
+    private String url;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public List<Product> getProductsById(List<Integer> ids) {
         if (ids == null || ids.size() == 0) return new ArrayList<>();
         HttpHeaders httpHeaders = new HttpHeaders();
         HttpEntity<List<Integer>> httpEntity = new HttpEntity<>(ids, httpHeaders);
-        Product[] products = restTemplate.postForObject(URL + "/products", httpEntity, Product[].class);
+        Product[] products = restTemplate.postForObject(url + "/products", httpEntity, Product[].class);
         if (products == null) throw new FailedRequestException("Failed to fetch products");
         return Arrays.asList(products);
     }
