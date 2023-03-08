f1432713fe7b624b6ce4b7e24dc6f235e7a96368
unknown <amsubhadeep@gmail.com>
2023-03-08 22:16:04 +0530

    Renamed project

--- src/main/java/com/website/backendtwo/exception/InvalidCredentialException.java
+++
@@ -1,7 +0,0 @@
-package com.website.backendtwo.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Invalid credentials")
-public class InvalidCredentialException extends RuntimeException {}
